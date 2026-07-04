8a2ebc3612480e6d62c0e0f71ffe2aa494356c5a
duckducksillygoose <Ashleigh.Gurr@student.curtin.edu.au>
2026-07-04 13:33:30 +0800

    wrote a function to represent the solved cube, as well as one-hot encode the stages. AG

---
+++ setup.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+COLORS = ["W", "R", "B", "O", "G", "Y"]  # defining the moves
+
+def solved_cube():
+    # 6 faces × 9 stickers each
+    return np.array([c for c in COLORS for _ in range(9)]) 
+
+def one_hot_encode(state):
+    mapping = {c: i for i, c in enumerate(COLORS)}
+    idxs = np.array([mapping[c] for c in state])
+    one_hot = np.eye(len(COLORS))[idxs]  # 1 dimensional vector
+    return one_hot.flatten()  

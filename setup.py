b89b2f1461b7bf4636b14b3bfe4f5635c4f83d8f
duckducksillygoose <Ashleigh.Gurr@student.curtin.edu.au>
2026-07-04 13:35:23 +0800

    changed the function so that the cube would print and I could test. AG

--- setup.py
+++ setup.py
@@ -4,10 +4,14 @@ COLORS = ["W", "R", "B", "O", "G", "Y"]  # defining the moves
 
 def solved_cube():
     # 6 faces × 9 stickers each
-    return np.array([c for c in COLORS for _ in range(9)]) 
+    print(np.array([c for c in COLORS for _ in range(9)]))
+    return
 
 def one_hot_encode(state):
     mapping = {c: i for i, c in enumerate(COLORS)}
     idxs = np.array([mapping[c] for c in state])
     one_hot = np.eye(len(COLORS))[idxs]  # 1 dimensional vector
     return one_hot.flatten()  
+
+
+solved_cube()
